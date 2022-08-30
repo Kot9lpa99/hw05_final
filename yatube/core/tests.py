2c3a8fccf43252ecaff6a7a969fbf1befce53608
Егор Дубихин <Dubihin.Egor@mail.ru>
2022-08-30 15:28:48 +0300

    final?

--- yatube/core/tests.py
+++ yatube/core/tests.py
@@ -4,9 +4,6 @@ from django.test import Client
 
 
 class ViewTestClass(TestCase):
-    def setUp(self):
-        self.client = Client()
-
     def test_error_page(self):
         response = self.client.get('/nonexist-page/')
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
