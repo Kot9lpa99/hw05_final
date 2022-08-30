0466de7806872c5e578d16031bf44d43c8e8cc9f
Егор Дубихин <Dubihin.Egor@mail.ru>
2022-08-30 14:45:07 +0300

    final

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -37,16 +37,16 @@ FORM_DATA_3 = {'username': AUTHOR,
                'password2': '1qaz2wsxC'}
 FORM_DATA_4 = {'text': 'Просто тест'}
 SMALL_GIF = (
-        b'\x47\x49\x46\x38\x39\x61\x02\x00'
-        b'\x01\x00\x80\x00\x00\x00\x00\x00'
-        b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-        b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-        b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-        b'\x0A\x00\x3B')
+    b'\x47\x49\x46\x38\x39\x61\x02\x00'
+    b'\x01\x00\x80\x00\x00\x00\x00\x00'
+    b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+    b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+    b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+    b'\x0A\x00\x3B')
 UPLOAD = SimpleUploadedFile(
-        name='small.gif',
-        content=SMALL_GIF,
-        content_type='image/gif')
+    name='small.gif',
+    content=SMALL_GIF,
+    content_type='image/gif')
 FORM_DATA_IMG = {'author': 'Автор',
                  'text': 'Тестовый текст',
                  'image': UPLOAD}
--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -48,16 +48,16 @@ FORM_FIELD = {
     'group': forms.models.ModelChoiceField,
     'image': forms.fields.ImageField}
 SMALL_GIF = (
-        b'\x47\x49\x46\x38\x39\x61\x02\x00'
-        b'\x01\x00\x80\x00\x00\x00\x00\x00'
-        b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-        b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-        b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-        b'\x0A\x00\x3B')
+    b'\x47\x49\x46\x38\x39\x61\x02\x00'
+    b'\x01\x00\x80\x00\x00\x00\x00\x00'
+    b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+    b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+    b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+    b'\x0A\x00\x3B')
 UPLOAD = SimpleUploadedFile(
-        name='small.gif',
-        content=SMALL_GIF,
-        content_type='image/gif')
+    name='small.gif',
+    content=SMALL_GIF,
+    content_type='image/gif')
 FORM_DATA_IMG = {'author': 'Автор',
                  'text': 'Тестовый текст',
                  'image': UPLOAD}
