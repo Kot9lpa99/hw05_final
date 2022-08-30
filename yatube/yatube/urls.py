94031efa5a9b33d39d26fc8f0e209c31a3ae567e
Егор Дубихин <Dubihin.Egor@mail.ru>
2022-08-30 15:43:24 +0300

    final?

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('about/', include('about.urls', namespace='about')),
 ]
 
-""" handler404 = 'core.views.page_not_found' """
+handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.server_error'
 handler403 = 'core.views.permission_denied'
 if settings.DEBUG:
