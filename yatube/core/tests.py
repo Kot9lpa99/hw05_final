3586b036ab26a3ba2a5df9c8bba0e280dc8a27eb
Егор Дубихин <Dubihin.Egor@mail.ru>
2022-08-30 15:42:59 +0300

    final?

--- yatube/core/tests.py
+++ yatube/core/tests.py
@@ -1,8 +1,13 @@
 from http import HTTPStatus
-from django.test import TestCase
+from django.test import TestCase, Client
 
 
 class ViewTestClass(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.client = Client()
+
     def test_error_page(self):
         response = self.client.get('/nonexist-page/')
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
